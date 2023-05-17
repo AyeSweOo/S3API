2c24a6b6ee721a9a2b97525638a4426f88bd927a
AyeSweOo <ayesweoo1990@gmail.com>
2023-05-18 00:04:38 +0630

    Add files via upload

---
+++ S3API/Controllers/APIDbContext .cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Gigz_API.Models;
+using Microsoft.Extensions.Configuration;
+using S3API.Models;
+//using Gigz.Entities;
+
+namespace Gigz_API.Models
+
+{
+    public class APIDbContext : DbContext
+    {
+
+        protected readonly IConfiguration Configuration;
+
+        public APIDbContext(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            // connect to sql server database
+            options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
+        }
+
+        public DbSet<PayloadData> PayloadData { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+            base.OnModelCreating(modelBuilder);
+
+
+
+
+        }
+
+    }
+}
---
+++ S3API/S3ExceptionMiddleware.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace S3API
+{
+    using Microsoft.AspNetCore.Builder;
+    using Microsoft.AspNetCore.Http;
+    using System;
+    using System.Net;
+    using System.Text.Json;
+    using System.Threading.Tasks;
+
+    public class S3ExceptionMiddleware
+    {
+        private readonly RequestDelegate next;
+
+        public S3ExceptionMiddleware(RequestDelegate next)
+        {
+            this.next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await next(context);
+            }
+            catch (Exception ex)
+            {
+                await HandleExceptionAsync(context, ex);
+            }
+        }
+
+        private static Task HandleExceptionAsync(HttpContext context, Exception exception)
+        {
+            context.Response.ContentType = "application/json";
+            context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+
+            var errorMessage = "Internal Server Error.";
+
+            // Check if the exception has a custom message
+            if (exception is ApplicationException appException)
+            {
+                errorMessage = appException.Message;
+            }
+
+            var response = new
+            {
+                StatusCode = context.Response.StatusCode,
+                Message = errorMessage
+            };
+            var jsonResponse = JsonSerializer.Serialize(response);
+
+            return context.Response.WriteAsync(jsonResponse);
+     
+        }
+
+    }
+
+    public static class ExceptionMiddlewareExtensions
+    {
+        public static void UseCustomExceptionMiddleware(this IApplicationBuilder app)
+        {
+            app.UseMiddleware<S3ExceptionMiddleware>();
+        }
+    }
+
+}
