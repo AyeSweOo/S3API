e24eacd51ad46512378fd20f76e03d522e42c90e
Administrator <Administrator@ASO>
2023-05-18 12:12:13 +0630

    Add project files.

---
+++ S3API/Controllers/PayloadController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Gigz_API.Models;
+using Microsoft.AspNetCore.Mvc;
+using S3API.Models;
+
+namespace S3API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PayloadController : ControllerBase
+    {
+
+        private readonly APIDbContext _context;
+        [Obsolete]
+        public PayloadController(APIDbContext context)
+        {
+            _context = context;
+        
+        }
+        [HttpPost]
+        public async Task<IActionResult> PostPayloadAsync(Payload payload)
+        {
+            try
+            {
+                //Receive the payload data
+                PayloadData  payloadData= payload.Data;
+                // Store the payload information in the database
+                _context.PayloadData.Add(payloadData);
+                await  _context.SaveChangesAsync();
+                return Ok(new {message = "Received payload and successfully saved." });
+            
+            }
+            catch (Exception ex)
+            {
+                // Handle exceptions and return appropriate response
+                 throw new ApplicationException("An error occurred while processing the request.");
+            }
+        }
+    }
+}
