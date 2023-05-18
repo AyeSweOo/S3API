72dc18bd508b997b6f6b2a1ebdde675db5aa8bad
AyeSweOo <ayesweoo1990@gmail.com>
2023-05-18 15:36:55 +0630

    Add files via upload

---
+++ S3API/Models/Payload1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace S3API.Models
+{
+    public class Payload1
+    {
+        public string deviceId { get; set; }
+        public string deviceType { get; set; }
+        public string deviceName { get; set; }
+        public string droupId { get; set; }
+        public string dataType { get; set; }
+        public PayloadData1 data { get; set; }
+        public long timestamp { get; set; }
+    }
+
+    public class PayloadData1
+    {
+        // Add properties based on the payload data structure
+        [Key]
+        public int Id { get; set; }
+        public bool fullPowerMode { get; set; }
+        public bool activePowerControl { get; set; }
+        public int firmwareVersion { get; set; }
+        public int temperature { get; set; }
+        public int humidity { get; set; }
+      
+    }
+   
+}
---
+++ S3API/Models/Payload2.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace S3API.Models
+{
+    public class Payload2
+    {
+        public string deviceId { get; set; }
+        public string deviceType { get; set; }
+        public string deviceName { get; set; }
+        public string groupId { get; set; }
+        public string dataType { get; set; }
+        public PayloadData2 data { get; set; }
+        public long timestamp { get; set; }
+    }
+    public class PayloadData2
+    {
+        // Add properties based on the payload data structure
+        [Key]
+        public int id { get; set; }
+        public int version { get; set; }
+        public string messageType { get; set; }
+        public int firmwareVersion { get; set; }
+        public int stateChanged { get; set; }
+  
+        public bool occupancy { get; set; }
+    }
+}
