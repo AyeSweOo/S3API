b2f02255d38c87f7c9ac99adf328119124229753
AyeSweOo <ayesweoo1990@gmail.com>
2023-05-18 15:56:39 +0630

    Add files via upload

---
+++ S3API/Models/APIDbContext .cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using S3API.Models;
+namespace S3API.Models
+{
+    public class APIDbContext : DbContext
+    {
+       protected readonly IConfiguration Configuration;
+
+        public APIDbContext(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            // connect to sql server database
+            options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
+        }
+        public DbSet<PayloadData1> PayloadData1 { get; set; }
+        public DbSet<PayloadData2> PayloadData2 { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+          base.OnModelCreating(modelBuilder);
+        }
+
+    }
+}
